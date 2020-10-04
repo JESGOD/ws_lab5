64d675adb91addc058d310b62654606614500dad
JESGOD <yonnyes@unicauca.edu.co>
2020-10-04 15:17:44 -0500

    se agregó el servicio web: consultar el primer documento de una coleccion

--- ws_lab5/src/main/java/control/ws_logica_tienda.java
+++ ws_lab5/src/main/java/control/ws_logica_tienda.java
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 package control;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
-/**
- *
- * @author zambo
- */
+
 public class ws_logica_tienda {
-    
+    public String consultarClientes(){
+        // creando cliente para la coneccion al servidor remoto
+        MongoClient cliente;
+        MongoClientURI uri = new MongoClientURI("mongodb://userLab5:passworduserLab5@93.188.167.110:27017/?authSource=lab5");
+        cliente = new MongoClient(uri);
+        // creamos objeto tipo MongoDatabase para conectarse y obtener la informacion de la base de datos que se encuentra en el servidor
+        MongoDatabase db;
+        db = cliente.getDatabase("lab5");
+        // obtenemos la collecion y retornamos el primer documento
+        MongoCollection<Document> coleccionTienda = db.getCollection("clientes");
+        return coleccionTienda.find().first().toJson();
+ //("mongodb://usuario:password@93.188.167.110:27017/?authSource=nombre_bd");
+    }
 }
--- ws_lab5/src/main/java/vista/Ws_tienda.java
+++ ws_lab5/src/main/java/vista/Ws_tienda.java
@@ -5,6 +5,7 @@
  */
 package vista;
 
+import control.ws_logica_tienda;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
@@ -12,6 +13,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -25,9 +27,17 @@ public class Ws_tienda {
     @Context
     private UriInfo context;
 
-    
     public Ws_tienda() {
+        
     }
-
     
+    @GET
+    @Path("consultarClientes")
+    @Produces({"applicaction/json"})
+    public String consultarClientes(){
+        ws_logica_tienda miTienda = new ws_logica_tienda();
+        // preguntar
+        return miTienda.consultarClientes();
+       
+    }
 }
